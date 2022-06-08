16ec4db283187789d112c20540b25f12908a1cd0
PTrixi <purpletrixi@gmail.com>
2022-06-08 18:10:34 +0200

    Changing some colors

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -13,18 +13,21 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_blue[]	    = "#346294";
 static const char col_red[]         = "#A23434";
 static const char col_red2[]        = "#B06262";
 static const char col_orange[]      = "#863D2E";
 static const char col_orange2[]     = "#D46D48";
 static const char col_pink[]        = "#AC3F6F";
+static const char col_pink2[]	    = "#C5618C";
 static const char col_green[]       = "#3E5645";
+static const char col_brown[]	    = "#93725f";
 static const char col_black[]       = "#000000";
 static const char col_white[]	    = "#FFFFFF";
 static const char *colors[][3]      = {
 	        /*               fg         bg         border   */
-	        [SchemeNorm] = { col_green, col_gray1, col_gray2 },
-		[SchemeSel]  = { col_black, col_green,  col_green  },
+	        [SchemeNorm] = { col_blue, col_gray1, col_gray2 },
+		[SchemeSel]  = { col_black, col_blue,  col_blue  },
 };
 
 
@@ -70,13 +73,15 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_black, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_blue, "-sf", col_black, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,	                XK_Return, spawn,          {.v = termcmd } },
+/*	{ MODKEY|ShiftMask,		XK_m,	   borderpx,	   {.i = +1 } },
+	{ MODKEY|ShiftMask,		XK_n,	   borderpx,	   {.i = -1 } }, */
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -113,7 +118,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_comma,      quit,           {0} },
 };
 
 /* button definitions */
--- dwm.c
+++ dwm.c
@@ -1547,6 +1547,19 @@ setgaps(const Arg *arg)
 	arrange(selmon);
 }
 
+Layout *last_layout;
+void
+fullscreen(const Arg *arg)
+{
+	if (selmon->showbar) {
+		for(last_layout = (Layout *)layouts; last_layout != selmon->lt[selmon->sellt]; last_layout++);
+		setlayout(&((Arg) { .v = &layouts[2] }));
+	} else {
+		setlayout(&((Arg) { .v = last_layout }));
+	}
+	togglebar(arg);
+}
+
 void
 setlayout(const Arg *arg)
 {
